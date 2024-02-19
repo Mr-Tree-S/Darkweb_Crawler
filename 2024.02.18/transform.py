7d5725a29ae1530db03497c702d73a75ce3a1be3
Ripper-S <ripper-s@ripper-s.com>
2024-02-19 11:18:56 +0800

    add transform

---
+++ 2024.02.18/transform.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import os
+import argparse
+
+def transfer(input_file):
+    # Read text file into DataFrame and specify column names
+    df = pd.read_csv(input_file, sep='|', header=None, names=['file_path', 'name', 'file_type', 'file_type_detail', 'date_time', 'file_size'])
+
+    # Convert DataFrame to Excel format
+    output_file = os.path.splitext(input_file)[0] + ".xlsx"
+    df.to_excel(output_file, index=False)
+
+# Set up argument parser
+parser = argparse.ArgumentParser(description='Transform text to Excel')
+parser.add_argument('-f', '--file', type=str, help='Input file path')
+args = parser.parse_args()
+
+# Execute transformation
+if args.file:
+    transfer(args.file)
+else:
+    print('Usage: python3 transform.py -f <file>')
