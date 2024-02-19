a74c82333febd18c6d50648cca6aae7eecb39793
Ripper-S <ripper-s@ripper-s.com>
2024-02-19 11:27:34 +0800

    11111

--- 2024.02.18/demo.py
+++ 2024.02.18/demo.py
@@ -6,7 +6,5 @@ href = "1.%20%E3%80%8A2022%E7%89%88%E9%A2%84%E7%AE%97%E6%A8%A1%E5%9E%8B2023-2030
 # 解码URL编码的字符串
 decoded_href = unquote(href)
 
-# 将解码后的结果进行UTF-8解码
-decoded_href_utf8 = decoded_href
 
-print(decoded_href_utf8)
+print(decoded_href)
